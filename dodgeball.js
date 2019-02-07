0ce8376a37fbcbb7f26740d8b992f2f600db180d
jakeburns <jakeburns@MacBook-Pro-4.local>
2019-02-07 18:23:28 -0500

    player now moves on click

--- dodgeball.js
+++ dodgeball.js
@@ -1,9 +1,6 @@
-var player_real = {
-  pos: 0
-}
+
 
 var moveUp = function(){
-  var player = document.getElementById('player');
-  player_real.pos += 200
-  player.style.top = player_real.pos
+  var player = document.getElementById("playerBtn");
+  player.style.top+="300px";
 }
