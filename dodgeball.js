7ae32d8547db9344d9794e65cf700aae5115d790
jakeburns <jakeburns@MacBook-Pro-4.local>
2019-02-07 19:35:35 -0500

    player now moves once up and down (w,s keys)

--- dodgeball.js
+++ dodgeball.js
@@ -1,6 +1,31 @@
 
+window.addEventListener("keydown", checkKeyPressed, false);
+
+
+
+function checkKeyPressed(e) {
+    if (e.keyCode == "83") {
+      //  alert("The 'a' key is pressed.");
+        //window.alert("Testing");
+      downPressed();
+    }
+    else if (e.keyCode == "87"){
+      upPressed();
+    }
+}
 
 var moveUp = function(){
   var player = document.getElementById("playerBtn");
   player.style.top+="300px";
+}
+
+var downPressed = function(){
+  var player = document.getElementById("playerBtn");
+   player.style.top =+ 200 + 'px';
+}
+
+var upPressed = function(){
+  var player = document.getElementById("playerBtn");
+   player.style.top =- 200 + 'px';
+
 }
