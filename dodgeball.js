ba85fb08521a6fbfc6532684c852e379e909c803
jakeburns <jakeburns@MacBook-Pro-4.local>
2019-02-07 19:56:03 -0500

    up and down work once

--- dodgeball.js
+++ dodgeball.js
@@ -2,28 +2,42 @@
 window.addEventListener("keydown", checkKeyPressed);
 
 window.alert("Use the W and S keys to move up and down to dodge the  balls.");
+var player = document.getElementById("playerBtn");
 
-function checkKeyPressed(e) {
-    if (e.keyCode == "83") {
-      downPressed();
-    }
-    else if (e.keyCode == "87"){
-      upPressed();
-    }
-}
+// function checkKeyPressed(e) {
+//     if (e.keyCode == "83") {
+//       downPressed();
+//     }
+//     else if (e.keyCode == "87"){
+//       upPressed();
+//     }
+//}
+
+
+function checkKeyPressed(e){
+		var key_code=e.which||e.keyCode;
+		switch(key_code){
+			case 83: //s key
+				downPressed();
+				break;
+			case 87: //w key
+				upPressed();
+				break;
+		}
+  }
 
 var moveUp = function(){
-  var player = document.getElementById("playerBtn");
-  player.style.top+="300px";
+
+  //player.style.top+="300px";
+  player.style.top=parseInt(player.style.top)-5 +'px';
 }
 
 var downPressed = function(){
-  var player = document.getElementById("playerBtn");
    player.style.top =+ 200 + 'px';
 }
 
 var upPressed = function(){
-  var player = document.getElementById("playerBtn");
    player.style.top =- 200 + 'px';
 
 }
+window.onload = init;
