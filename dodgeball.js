db877d8c2fa0a84e236f6afc9443e2e42babd424
KingLoneCat <jibraankhan@KingLoneCats-MacBook-Air.local>
2019-02-09 16:45:48 -0500

    Productive day.

--- dodgeball.js
+++ dodgeball.js
@@ -1,15 +1,17 @@
-
 window.addEventListener("keydown", checkKeyPressed);
-var player = {
+
+var real_player = {
   x: 0,
   y: 0
 }
 var step = 20;
+var secs = 1;
+var timer = 0;
 
-window.alert("Use the W and S keys to move up and down to dodge the  balls.");
+window.alert("Use WASD to move the character around. The game's objective is to gather the most coins in the given time.");
 var player = document.getElementById("player");
 var player_height = player.getAttribute("height")
-var screen_height = 600
+var screen_height = 450
 
 console.log(player_height)
 // function checkKeyPressed(e) {
@@ -35,21 +37,65 @@ function checkKeyPressed(e){
 			case 87: //w key
 				upPressed();
 				break;
+      case 65:
+        leftPressed();
+        break;
+      case 68:
+        rightPressed();
+        break;
 		}
-  }
-
-var moveUp = function(){
-
-  //player.style.top+="300px";
-  player.style.top=parseInt(player.style.top)-5 +'px';
 }
 
 var downPressed = function(){
-  player_position += step;
-  player.style.top = player_position + 'px';
+  if (real_player.y <= screen_height){
+    real_player.y += 20;
+    player.style.top = real_player.y + 'px';
+  }
 }
 
 var upPressed = function(){
-  player_position -= step
-  player.style.top = player_position + 'px';
+  if (real_player.y >= 0){
+    real_player.y -= 10;
+    player.style.top = real_player.y + 'px';
+  }
+}
+
+var leftPressed = function(){
+  if (real_player.x >= 0){
+    real_player.x -= 10;
+    player.style.left = real_player.x + 'px';
+  }
+}
+
+var rightPressed = function(){
+  if (real_player.x <= 1300){
+    real_player.x += 10;
+    player.style.left = real_player.x + 'px';
+  }
+}
+
+var generateCoin = function(){
+  console.log("Generating coin.");
+  var coin = document.createElement("IMG");
+//  random_x = (Math.random() * 1300) + 1;
+//  random_y = (Math.random() * 450) + 1;
+
+  coin.setAttribute("src", "images/Substitute.png");
+  coin.setAttribute("width", "35px");
+  coin.setAttribute("height", "35px");
+  coin.setAttribute("alt", 'Coin');
+  coin.setAttribute('id', 'Coin')
+  document.body.appendChild(coin);
+
+//  console.log('(' + random_x + ', ' + random_y + ')');
+
+}
+
+
+
+
+while (timer < 300){
+  setTimeout(func, 1/60)
+  console.log("Game has ended.")
+
 }
